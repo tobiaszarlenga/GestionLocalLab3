dea9e1d11084fb9a22da39123129a70024a08103
Mariano Achi <achimariano@gmail.com>
2025-05-27 20:23:33 -0300

    Creo la clase DetalleVenta

---
+++ GestioLocalLab3.API/Models/DetalleVenta.cs
@@ -0,0 +1,14 @@
+﻿namespace GestioLocalLab3.API.Models
+{
+    public class DetalleVenta
+    {
+        public int ID { get; set; }
+        public int VentaId { get; set; }
+        public Venta Venta { get; set; }
+        public int ProductoID { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+    }
+}
