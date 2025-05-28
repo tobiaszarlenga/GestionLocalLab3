34b158660d631f78a92d5cf57a66fc32c460910d
Pablo Cesar Gramajo <pgramajo23@gmail.com>
2025-05-28 11:28:31 -0300

    add: venta controller y su interfaces aplicando el principio SRP

---
+++ GestioLocalLab3.API/Reporsitories/VentaRepository.cs
@@ -0,0 +1,29 @@
+﻿using GestioLocalLab3.API.Interface;
+using GestioLocalLab3.API.Models;
+
+namespace GestionLocalLab3.API.Repositories
+{
+    public class VentaRepository : IVentaRepository
+    {
+        private readonly List<Venta> _ventas = new();
+        private int _nextId = 1;
+
+        public List<Venta> ObtenerTodas()
+        {
+            return _ventas;
+        }
+
+        public Venta? ObtenerPorId(int id)
+        {
+            return _ventas.FirstOrDefault(v => v.Id == id);
+        }
+
+        public void Agregar(Venta venta)
+        {
+            venta.Id = _nextId++;
+            venta.Fecha = DateTime.Now;
+            venta.MontoTotal = venta.Detalles.Sum(d => d.Cantidad * d.PrecioUnitario);
+            _ventas.Add(venta);
+        }
+    }
+}
