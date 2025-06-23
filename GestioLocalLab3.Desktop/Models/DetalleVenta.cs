13774e0d9ca41cecd04866696eaf79c377ca4b01
Aitana Molina Juarez <molinajuarezaitana@gmail.com>
2025-06-23 15:47:24 -0300

    feat: Agregue carpeta models con clases en Desktop

---
+++ GestioLocalLab3.Desktop/Models/DetalleVenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioLocalLab3.Desktop.Models
+{
+    public class DetalleVenta
+    {
+        public int ProductoID { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+
+}
---
+++ GestioLocalLab3.Desktop/Models/Venta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioLocalLab3.Desktop.Models
+{
+    public class Venta
+    {
+        public DateTime Fecha { get; set; }
+        public string MetodoPago { get; set; } = string.Empty;
+        public decimal MontoTotal { get; set; }
+        public List<DetalleVenta> Detalles { get; set; } = new();
+    }
+}
