a2064689274cf9531c15fa18ab8acc6b08c258aa
Tobias Zarlenga <tobizarlenga@gmail.com>
2025-06-26 16:55:15 -0300

    feat: sesion.cs para usuario

--- GestioLocalLab3.Desktop/FormLogin.cs
+++ GestioLocalLab3.Desktop/FormLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GestioLocalLab3.Desktop.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
---
+++ GestioLocalLab3.Desktop/Models/Sesion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioLocalLab3.Desktop.Models
+{
+    internal class Sesion
+    {
+        public static string? UsuarioNombre { get; set; }
+    }
+}
